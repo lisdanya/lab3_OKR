637135695d1c52cc25420fd12687cb49cf200c5f
Daniel <52752918+lisdanya@users.noreply.github.com>
2020-10-20 21:46:01 +0300

    toTop

--- src/script.js
+++ src/script.js
@@ -1,11 +1,19 @@
-let jopa = document.querySelector('.menu-open');
+let button = document.querySelector('.menu-open');
 let menu = document.querySelector('.menu');
 let lil = document.getElementsByClassName('lil')
-jopa.onclick = function() {
-    // jopa.classList.toggle('show-menu');
+let toTop = document.getElementById('idTop')
+let header = document.getElementById('header')
+button.onclick = function() {
     menu.classList.toggle('show-menu');
     for (let i = 0; i < lil.length; i++) {
         lil[i].classList.toggle('show-lil')
     }
 }
+toTop.onclick = function() {
+    header.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start'
+    })
+}
+
 
